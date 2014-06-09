a5adab99efbbf00f0034e28f76332b36a329aaed
Sascha Häusler <saeschdivara@gmail.com>
2014-06-09 10:13:29 +0200

    Added settings file template

---
+++ local_settings.tpl.py
@@ -0,0 +1,24 @@
+# Path to your repository with the .git path
+REPOSITORY_PATH = '/my/example/repository/path'
+
+# List of all paths which have to be searched for analytics
+SEARCHING_PATHS = (
+    '',
+)
+
+# Endings of files which are in the searching paths and should be
+# includes in the analytics process
+ALLOWED_ENDINGS = (
+    'py',
+)
+
+# List of regex patterns of files or paths which should not be included
+# in the analytics
+EXCLUDE_PATTERNS = (
+    '',
+)
+
+# Lists of static paths which should be excluded from the analytics
+EXCLUDE_PATHS = (
+    '',
+)
